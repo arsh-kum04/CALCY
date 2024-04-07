07434119714e2edba978ea8ef2cd597feba16049
Arsh Kumar <arshkum18@gmail.com>
2024-04-07 13:46:45 +0530

    Add documentation for ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/calcy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/calcy/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
-```java
-// Folder Name: test
-// File Name: ExampleInstrumentedTest.java
+**Folder Name:** test
+**File Name:** ExampleInstrumentedTest.java
 
+```java
+// Import required libraries for testing.
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -13,16 +14,17 @@ import org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
+ * Used for testing the functionality of Android applications. Runs on a physical or virtual Android device.
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Get the context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        // Package name of the app under test.
+        // Assertion to verify the package name of the app under test.
         assertEquals("com.example.calcy", appContext.getPackageName());
     }
 }
