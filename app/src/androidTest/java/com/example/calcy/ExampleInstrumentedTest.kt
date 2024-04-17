2590d021b362cec043e8554d5c7887aefaa00b96
Arsh Kumar <arshkum18@gmail.com>
2024-04-17 12:22:37 +0530

    Add documentation for ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/calcy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/calcy/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@
 **File Name:** ExampleInstrumentedTest.java
 
 **Line by Line Documented Code:**
+
 ```java
 // Import required libraries for testing.
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -10,37 +11,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.Assert;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * Used for testing the functionality of Android applications. Runs on a physical or virtual Android device.
- * See [testing documentation](http://d.android.com/tools/testing).
- *
- * @RunWith(AndroidJUnit4.class): Tells JUnit to run this class as an Android instrumented test.
- */
-// This line annotates the class with @RunWith(AndroidJUnit4.class), which tells JUnit to run this class as an Android instrumented test.
-// Instrumented tests run on a physical or virtual Android device and have access to the Android framework APIs.
+// @RunWith(AndroidJUnit4.class): Tells JUnit to run this class as an Android instrumented test.
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    /**
-     * useAppContext() is a method in the ExampleInstrumentedTest class that is annotated with @Test.
-     *
-     * @Test: Indicates that this method should be run as a test.
-     *
-     * This method tests if the context of the app under test is correct by using the InstrumentationRegistry.getInstrumentation().getTargetContext() method to get the context of the app under test.
-     *
-     * @assertEquals(String expected, String actual): Asserts that the expected and actual values are equal.
-     */
+    // @Test: Indicates that this method should be run as a test.
     @Test
     public void useAppContext() {
         // Get the context of the app under test.
-        // This line gets the context of the app under test by calling InstrumentationRegistry.getInstrumentation().getTargetContext().
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         // Assertion to verify the package name of the app under test.
-        // This line checks if the package name of the app under test matches the expected package name ("com.example.calcy") using the assertEquals() method.
-        // assertEquals() is a method from the JUnit Assert class that checks whether two values are equal.
         assertEquals("com.example.calcy", appContext.getPackageName());
     }
 }
